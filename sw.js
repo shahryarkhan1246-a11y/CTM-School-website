e28629f17b7c7d4e135d6a61ab0d9a6702354fc1
shahryarkhan1246-a11y <shahryarkhan1246@gmail.com>
2026-08-16 14:13:44 +0500

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "ctm-school-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
